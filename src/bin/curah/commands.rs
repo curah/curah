71efe685c8528f4b80286c67c7742bc0cd15eaca
Emil Walser <emil.walser@gmail.com>
2022-02-03 11:56:02 +0100

    lol lol

--- src/bin/curah/commands.rs
+++ src/bin/curah/commands.rs
@@ -1,18 +1,20 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 mod project;
 
 #[derive(Parser)]
 struct CurahCli {
+    #[clap(subcommand)]
     command: CurahCommand,
 }
 
+#[derive(Subcommand)]
 enum CurahCommand {
     Project,
 }
 
 pub fn run() -> Result<(), String> {
     match CurahCli::parse().command {
-        CurahCommand::Project => project::run()?,
+        CurahCommand::Project => project::run(),
     }
 }
